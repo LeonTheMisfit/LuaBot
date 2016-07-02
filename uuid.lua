e4aa1c43f2815e5980ef7f8b45d2474ebc5e99a6
Leon N <leon.newswanger@gmail.com>
2016-07-02 12:31:25 -0400

    Added uuid module for creating IDs.

---
+++ uuid.lua
@@ -0,0 +1,18 @@
+local chars = {
+  "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+}
+
+local uuid = {}
+
+function uuid.generate(len)
+  len = len or config.idlen
+  local id = ""
+  for _ = 1, len do
+    local c = chars[math.random(1, #chars)]
+    id = id .. c
+  end
+  return id
+end
+
+return uuid
