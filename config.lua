2c20ac4799ff72b1e2b6cd61424a5843851911ec
Leon N <leon.newswanger@gmail.com>
2016-07-07 21:12:06 -0400

    Updated config for testing.

--- config.lua
+++ config.lua
@@ -3,13 +3,12 @@ return {
   port = 6667,
   chans = { "#totallynotrobots" },
   send_limit = 4,
-  nick = "LuaBotPlusPlus",
+  nick = "LuaBotTesting",
   password = "password",
   user = "A new improved LuaBot is on the way",
   idlen = 16,
   db_file = "db/main.db",
   flag_time = 5 * 60,
   admins = { "leonthemisfit" },
-  ops = { "leonthemisfit" },
   pastebin_key = "ee511ff62870c5b6d209bb130fa7433d"
 }
